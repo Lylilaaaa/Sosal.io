698f04bad1c36f46bc945fae8c53ed79597a8cd7
zhujian <zhujian@benshiboss.com>
2025-03-26 08:33:58 +0800

    right nav ing

--- font-code/src/components/RightBar.vue
+++ font-code/src/components/RightBar.vue
@@ -2,12 +2,60 @@
   <div class="right-container">
     <div class="sosoEarn">
       <div class="avatar">
-        <img src="/ui/Asset 1.svg" alt="" />
+        <img class="avatar1" src="/ui/Asset 1.svg" alt="" />
         <div class="text">sosoEARN</div>
+        <img class="avatar2" src="/ui/Asset 73.svg" alt="" />
       </div>
-      <div class="info"></div>
+      <div class="info">
+        <div class="firstLine">
+          <span>Ee7v…Rk</span>
+          <img src="/ui/Asset 76.svg" alt="" />
+          <img src="/ui/Asset 77.svg" alt="" />
+          <img src="/ui/Asset 78.svg" alt="" />
+          <img src="/ui/Asset 78.svg" alt="" />
+          <img class="share" src="/ui/Asset 57.svg" alt="" />
+        </div>
+        <div class="secondLine">
+          <span class="smallText">7d Pnl</span>
+          <span class="bigTextGreen">+1,238%</span>
+          <span class="smallText">win Rate</span>
+          <span class="bigText">78%</span>
+        </div>
+        <div class="thirdLine">
+          <span class="smallText">Referral:</span>
+          <img src="/ui/Asset 74.svg" alt="" />
+          <span class="smallText bold">27</span>
+          <span class="smallText">Fees Rebat:</span>
+          <img src="/ui/Asset 75.svg" alt="" />
+          <span class="smallText bold">1.2815</span>
+          <span class="smallText">Points:</span>
+          <span class="smallText bold">128,870</span>
+        </div>
+      </div>
     </div>
-    <div class="buy"></div>
+    <div class="buy">
+      <div class="buyTop">
+        <div class="defaultButton selectButton">BUY</div>
+        <div class="defaultButton">SELL</div>
+      </div>
+      <div class="buyContainer">
+        <div class="buyNav">
+          <a class="select" href="">BUY NOW</a>
+          <a href="">BUY DIP</a>
+        </div>
+        <div class="selectWallet">
+          <div class="selectWalletTop">
+            <img class="wallet" src="/ui/Asset 79.svg" alt="" />
+            <span class="text">Select Wallet</span>
+            <img class="select" src="/ui/Asset 80.svg" alt="" />
+          </div>
+          <span>BAL:12.2349 SOL</span>
+        </div>
+      </div>
+      <div class="buySetting"></div>
+      <div class="buyBottom"></div>
+    </div>
+
     <div class="sportLight"></div>
   </div>
 </template>
@@ -21,7 +69,7 @@
 
 .sosoEarn {
   height: 70px;
-  background-color: #181616;
+  background-color: #141212;
   margin-bottom: 8px;
   border-radius: 8px;
   display: flex;
@@ -48,6 +96,7 @@
   color: transparent;
   padding: 2px;
   top: -13px;
+  font-weight: bold;
 }
 
 .avatar .text::before {
@@ -69,13 +118,152 @@
   border-image: linear-gradient(45deg, #e319a7, #9804dc, #4404c3) 1;
 }
 
-.info {
+.avatar img.avatar2 {
+  width: 15px;
+  position: relative;
+  top: -30px;
+  left: 70px;
 }
 
+.info {
+  flex: auto;
+  border-radius: 0 8px 8px 0;
+  padding: 8px;
+  margin-left: 17px;
+}
+
+.info .firstLine {
+  font-size: 12px;
+  color: #fff;
+  font-weight: bold;
+  display: flex;
+  gap: 4px;
+  justify-content: space-between;
+  align-items: end;
+}
+
+.firstLine img {
+  height: 12px;
+}
+
+.info .firstLine .share {
+  height: 16px;
+  flex: auto;
+  margin-left: auto;
+  flex-grow: 0;
+}
+
+.smallText {
+  font-size: 8px;
+  color: #b2a18f;
+}
+
+.bigText {
+  font-size: 18px;
+  color: #b2a18f;
+  font-weight: bold;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.bigTextGreen {
+  font-size: 18px;
+  color: #2bffbc;
+  font-weight: bold;
+}
+.secondLine {
+  font-size: 8px;
+  display: flex;
+  gap: 4px;
+  margin-top: 4px;
+}
+
+.thirdLine {
+  display: flex;
+  gap: 3px;
+  margin-top: 8px;
+  white-space: nowrap;
+}
+
+.thirdLine img {
+  height: 5px;
+  vertical-align: middle;
+  margin-top: 2px;
+}
 .buy {
   background-color: #181616;
   margin-bottom: 8px;
   border-radius: 8px;
+  padding: 8px;
+}
+.buyTop {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 8px;
+}
+
+.defaultButton {
+  font-size: 14px;
+  height: 38px;
+  border: 1px solid #b2a18f;
+  flex-grow: 1;
+  text-align: center;
+  color: #b2a18f;
+  line-height: 38px;
+  border-radius: 8px;
+  /* font-weight: bold; */
+}
+
+.buyNav a {
+  font-size: 12px;
+  color: white;
+  text-decoration: none;
+  margin-right: 20px;
+}
+
+.buyNav a.select {
+  font-weight: bold;
+}
+
+.selectButton {
+  border-color: #ff8000;
+  color: #ff8000;
+  font-weight: bold;
+}
+.selectWallet {
+  display: flex;
+  border-radius: 4px;
+  margin: 8px 0;
+  justify-content: space-between;
+  align-items: end;
+}
+.selectWalletTop {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 5px;
+  width: 150px;
+  height: 27px;
+  color: #b2a18f;
+  border: 1px solid #b2a18f;
+  line-height: 27px;
+  border-radius: 4px;
+}
+
+.selectWalletTop img {
+  width: 13px;
+}
+
+.selectWalletTop .wallet {
+  margin-left: 3px;
+}
+.selectWalletTop .select {
+  margin-left: auto;
+  margin-right: 5px;
 }
 
 .sportLight {
